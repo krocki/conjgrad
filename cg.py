73dae2d7e496fcb1be985fe9cc0a1f2c6895355f
Kamil Rocki <kmrocki@MacBook-Pro-5.local>
2020-02-06 14:48:14 -0800

    cg

--- cg.py
+++ cg.py
@@ -5,7 +5,7 @@ import time
 # x - initial solution
 # tol - err tolerance
 # max_iter
-def conjugate_gradients(A, b, x, tol=1e-8, max_iter=0):
+def conjugate_gradients(A, b, x, tol=1e-6, max_iter=10000):
 
   r = b - np.dot(A, x) # residual
   err = np.dot(r.T, r) # error
@@ -29,23 +29,47 @@ def conjugate_gradients(A, b, x, tol=1e-8, max_iter=0):
 
   return x, err, i
 
-if __name__ == "__main__":
+def solve(A, b):
 
-  #A = np.array([[4, 1],[1, 3]])
-  A = np.random.rand(10, 20)
-  #b = np.array([[1],[2]])
-  b = np.random.rand(10)
-
-  # initial solution
   x0 = np.zeros(A.shape[1])
   # apply A.T, make positive semidefinite
+  t0 = time.perf_counter()
   A0 = np.dot(A.T, A)
   b0 = np.dot(A.T, b)
-
-  t0 = time.perf_counter()
-  x, r, i = conjugate_gradients(A0, b0, x0)
   t1 = time.perf_counter()
+  x, r, i = conjugate_gradients(A0, b0, x0)
+  t2 = time.perf_counter()
 
-  print('x')
-  print(x)
-  print('i={}, r={}, t={:.6f} s'.format(i, r, t1-t0))
+  t = {"prepro" : t1-t0, "cg" : t2-t1}
+
+  return x, r, i, t
+
+if __name__ == "__main__":
+
+  A = np.random.rand(40, 50)
+  b = np.random.rand(40)
+  x,r,i,t = solve(A, b)
+
+  # verify
+  Ax = np.dot(A, x)
+  max_err = np.max(np.fabs(b - Ax))
+  err_norm = np.linalg.norm(b - Ax)
+  print('max err={}, err norm]=={}'.format(max_err, err_norm))
+  print('i={}, t={:.6f} s'.format(i, t['cg']))
+
+# use OCTAVE as ref
+  #A = np.loadtxt("A.txt")
+  #b = np.loadtxt("b.txt")
+  #x_ref = np.loadtxt("x.txt")
+  #t_ref = np.loadtxt("t.txt")
+
+  ## solution, residuals, iters, time
+  #x,r,i,t = solve(A, b)
+
+  #print('r={}'.format(r))
+
+  #max_err = np.max(np.fabs(x - x_ref))
+  #err_norm = np.linalg.norm(x - x_ref)
+
+  #print('max err={}, err norm]=={}'.format(max_err, err_norm))
+  #print('i={}, t={:.6f} s, ref t={:.6f} s'.format(i,t['cg'], t_ref))
